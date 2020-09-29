cb747230f7c77bf9edf8e77bb497c993779eedec
james.warren <james.warren@digital.justice.gov.uk>
2020-09-29 10:03:32 +0100

    IN-394 add step function to kick off jobs

--- casrec_data/create_db/create_casrec_db_tables.py
+++ casrec_data/create_db/create_casrec_db_tables.py
@@ -1,35 +1,25 @@
 import os
-import time
 import pandas as pd
 
-# import psycopg2
-# conn = psycopg2.connect("host=localhost port=6666 dbname=casrec user=casrec "
-#                         "password=casrec")
-# cur = conn.cursor()
-
-anon_data_dir = '../anon_data'
-sql_out = open('create_tables.sql', "w")
+anon_data_dir = "../anon_data"
+sql_out = open("create_tables.sql", "w")
 
 for file in os.listdir(anon_data_dir):
 
-    file_name = file.split('.')[0]
-
+    file_name = file.split(".")[0]
 
     df = pd.read_csv(os.path.join(anon_data_dir, file))
-    df_renamed = df.rename(columns ={'Unnamed: 0': "Record"})
+    df_renamed = df.rename(columns={"Unnamed: 0": "Record"})
 
     columns = [x for x in df_renamed.columns.values]
 
-    statement = f"CREATE TABLE \"{file_name}\" ("
+    statement = f'CREATE TABLE "{file_name}" ('
     for i, col in enumerate(columns):
-        statement += f"\"{col}\" text"
-        if i+1 < len(columns):
+        statement += f'"{col}" text'
+        if i + 1 < len(columns):
             statement += ","
 
     statement += "); \n\n\n"
     sql_out.write(statement)
 
-    # cur.execute(statement)
-
 sql_out.close()
-# conn.commit()
