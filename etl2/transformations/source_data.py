97fe01354b3d9d5ffda8842ec9bcdd29808fe5c5
jmackown <jmackown@users.noreply.github.com>
2020-10-09 10:25:16 +0100

    IN-405 - link some tables together (#16)
    * reorganise and added some linked tables
    * Swapped caseitem_note for person_not as per IN-414. 
    Also added person type as per IN-410

--- etl2/transformations/source_data.py
+++ etl2/transformations/source_data.py
@@ -1,27 +1,28 @@
 import pandas as pd
 import re
 
-class SourceData():
 
+class SourceData:
     def generate_select_string(self, mapping, source_table_name, limit=None):
-        cols = mapping[mapping['casrec_column_name'].notna()]
+        cols = mapping[mapping["casrec_column_name"].notna()]
 
-        cols = cols[cols['casrec_table'].str.lower() == source_table_name][[
-            'casrec_column_name', 'alias']]
+        cols = cols[cols["casrec_table"].str.lower() == source_table_name][
+            ["casrec_column_name", "alias"]
+        ]
 
         col_names_with_alias = cols.to_dict(orient="records")
 
         statement = "SELECT "
         for i, col in enumerate(col_names_with_alias):
-            if ',' in col['casrec_column_name']:
+            if "," in col["casrec_column_name"]:
 
-                statement += re.sub(r'(\w+)', r'"\1"', col['casrec_column_name'])
+                statement += re.sub(r"(\w+)", r'"\1"', col["casrec_column_name"])
             else:
                 statement += f"\"{col['casrec_column_name']}\" as \"{col['alias']}\""
             if i + 1 < len(col_names_with_alias):
-                statement += ', '
+                statement += ", "
             else:
-                statement += ' '
+                statement += " "
         statement += f"FROM etl1.{source_table_name} "
         if limit:
             statement += f"LIMIT {limit}"
