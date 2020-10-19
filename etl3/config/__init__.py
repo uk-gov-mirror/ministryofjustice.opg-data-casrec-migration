ca7a0a2ca31d717dd29f55b734214410a4636979
jackgoodby <jackgoodby@gmail.com>
2020-10-19 17:35:56 +0100

    IN-368 config in the style of ETL2 and updated DB field types to match inbound from ETL2

--- etl3/config/__init__.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-
-def get_config(env="local"):
-    config = configparser.ConfigParser()
-    if env == "local":
-        config.read("etl3/config/local_dev.cfg")
-
-    return config
