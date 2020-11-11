18460adb7756c44cad30b3d05eb3ec05856c5c5b
jmackown <jmackown@users.noreply.github.com>
2020-11-11 16:17:14 +0000

    IN-497 - mapping summary and tidy up (#54)
    * tidy mapping up a bit
    add summary
    add diff
    copy to migration steps
    
    * run new mapping
    
    * created date not mapped yet
    
    * consistent-ising
    
    * turns out if you spontaneously remove createddate then things break...

--- docs/support_scripts/mapping_doc_to_json/app.py
+++ docs/support_scripts/mapping_doc_to_json/app.py
@@ -1,6 +1,12 @@
 import click as click
-
+import shutil
+import os
 from mapping_all_sheets import Mapping
+
+
+steps_root = "../../../migration_steps/"
+folder_name = "mapping_definitions"
+steps_to_copy_to = ["transform_casrec", "acquire_target_ids"]
 
 
 @click.command()
@@ -12,16 +18,27 @@ from mapping_all_sheets import Mapping
 @click.option(
     "--output_dir",
     prompt="path to the output directory",
-    default="../../../migration_steps/transform_casrec/app/mapping_definitions",
+    default="./docs/mapping_definitions",
 )
-def main(mapping_doc, output_dir):
+@click.option(
+    "--move_to_migration_steps",
+    prompt=f"automatically copy to {', '.join(steps_to_copy_to)}?",
+    default=True,
+)
+def main(mapping_doc, output_dir, move_to_migration_steps):
 
     mapping = Mapping(excel_doc=mapping_doc, output_folder=output_dir)
     mapping.generate_json_files()
+
+    if move_to_migration_steps:
+        for step in steps_to_copy_to:
+            shutil.copytree(
+                output_dir,
+                os.path.join(steps_root, step, "app", folder_name),
+                dirs_exist_ok=True,
+            )
 
 
 if __name__ == "__main__":
 
     main()
-
-    print("All finished :)")
--- docs/support_scripts/mapping_doc_to_json/mapping_all_sheets.py
+++ docs/support_scripts/mapping_doc_to_json/mapping_all_sheets.py
@@ -1,7 +1,12 @@
 import json
-from typing import List, Dict
 import os
+from datetime import datetime
+from typing import List, Dict
+
 import pandas as pd
+from deepdiff import DeepDiff
+
+pd.options.mode.chained_assignment = None
 
 
 class Mapping:
@@ -20,10 +25,27 @@ class Mapping:
             "is_pk",
             # 'fk_children',
             "fk_parents",
+            "is_complete",
         ]
         self.columns = columns if len(columns) > 0 else self.default_columns
+        self.diff = {
+            "generated_date": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "previous_generate_date": "",
+        }
+        self.summary = {
+            "worksheets": {},
+            "total": {
+                "worksheets": {"total_sheets": 0, "total_complete": 0,},
+                "fields": {
+                    "total_fields": 0,
+                    "total_unmapped": 0,
+                    "total_mapped": 0,
+                    "percentage_complete": 0,
+                },
+            },
+        }
 
-    def _apply_column_alias(self, df):
+    def _apply_column_alias(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Some source columns are used multiple times when mapping to the destination.
         This appends a number to the column alias for the duplicates, makes the
@@ -40,25 +62,111 @@ class Mapping:
 
     def _clean_up_and_convert_to_dict(self, df: pd.DataFrame) -> Dict:
         """
-        1. add the 'alias' col to the dataframe
-        2. only select the columns we are interested in
-        3. drop any rows that have no value in all interesting cols - means they've
-            probably not been mapped yet
-        4. fill 'nan' with empty string
-        5. set index
-        6. convert to dictionary
+        1.  add the 'alias' col to the dataframe
+        2.  only select the columns we are interested in
+        3.  remove False from 'is_pk' col
+        4.  convert 'is_complete' rows to boolean: True
+        5.  convert 'is_complete' rows to boolean: False
+        6.  drop any rows that have no value in all interesting cols
+        7.  fill 'nan' with empty string
+        8.  set 'include' to True where 'is_complete' is not N/A (ie, means it should
+            be mapped even if the mapping is not complete)
+        9.  force pk fields to be included
+        10. force fk link fields to be included
+        11. only select the rows where 'include' is True
+        12. drop the include col (because it's always True)
+        13. set index to the column name so the to_dict pivots on the sirius column name
+        14. convert to dictionary
         """
 
+        # add the 'alias' col to the dataframe
         mapping_df = self._apply_column_alias(df=df)
+        # only select the columns we are interested in
         mapping_df = mapping_df[self.columns + [self.index_column]]
+        # remove False from 'is_pk' col
+        mapping_df["is_pk"] = mapping_df.apply(
+            lambda x: True if x["is_pk"] is True else "", axis=1
+        )
+        # convert 'is_complete' rows to boolean: True
+        mapping_df["is_complete"] = mapping_df.apply(
+            lambda x: True if x["is_complete"] in ["yes", "YES"] else x["is_complete"],
+            axis=1,
+        )
+        # convert 'is_complete' rows to boolean: False
+        mapping_df["is_complete"] = mapping_df.apply(
+            lambda x: False if x["is_complete"] in ["no", "NO"] else x["is_complete"],
+            axis=1,
+        )
+        # drop any rows that have no value in all interesting cols
         mapping_df = mapping_df.dropna(axis=0, how="all", subset=self.columns)
+        # fill 'nan' with empty string
         mapping_df = mapping_df.fillna("")
+        # set 'include' to True where 'is_complete' is not N/A (ie, means it should be
+        # mapped even if the mapping is not complete)
+        mapping_df["include"] = mapping_df.apply(
+            lambda x: True if x["is_complete"] != "" else False, axis=1
+        )
+        # force pk fields to be included
+        mapping_df["include"] = mapping_df.apply(
+            lambda x: True if x["is_pk"] is True else x["include"], axis=1
+        )
+        # force fk link fields to be included
+        mapping_df["include"] = mapping_df.apply(
+            lambda x: True if x["fk_parents"] != "" else x["include"], axis=1
+        )
+        # only select the rows where include is True
+        mapping_df = mapping_df.loc[mapping_df["include"] == True]
+        # drop the include col (because it's always True)
+        mapping_df.drop("include", axis=1, inplace=True)
+        # print(mapping_df.to_markdown())
+        # set index to the column name so the to_dict pivots on the sirius column name
         mapping_df = mapping_df.set_index(self.index_column)
+        # convert to dictionary
         mapping_dict = mapping_df.to_dict("index")
 
         return mapping_dict
 
-    def _format_multiple_columns(self, mapping_dict: Dict):
+    def _add_single_module_details_to_summary(
+        self, module_name: str, mapping_dict: dict
+    ):
+
+        module_total_rows = len(mapping_dict)
+        module_total_unmapped_rows = len(
+            [k for k, v in mapping_dict.items() if v["is_complete"] is False]
+        )
+        module_total_mapped_rows = module_total_rows - module_total_unmapped_rows
+        module_percentage_complete = round(
+            module_total_mapped_rows / module_total_rows * 100
+        )
+
+        self.summary["worksheets"][module_name] = {}
+        self.summary["worksheets"][module_name]["total_rows"] = module_total_rows
+        self.summary["worksheets"][module_name][
+            "total_unmapped"
+        ] = module_total_unmapped_rows
+        self.summary["worksheets"][module_name][
+            "total_mapped"
+        ] = module_total_mapped_rows
+        self.summary["worksheets"][module_name][
+            "percentage_complete"
+        ] = module_percentage_complete
+
+        fields = self.summary["total"]["fields"]
+        sheets = self.summary["total"]["worksheets"]
+
+        fields["total_fields"] += module_total_rows
+        fields["total_unmapped"] += module_total_unmapped_rows
+        fields["total_mapped"] += module_total_mapped_rows
+        fields["percentage_complete"] = round(
+            fields["total_mapped"] / fields["total_fields"] * 100
+        )
+
+        sheets["total_sheets"] += 1
+        sheets["total_complete"] = (
+            sheets["total_complete"] + 1 if module_percentage_complete == 100 else +0
+        )
+
+    def _format_multiple_columns(self, mapping_dict: Dict) -> Dict:
         """
         Some items are across multiple columns in the source data but map to a single
         column in the destination.
@@ -76,7 +184,7 @@ class Mapping:
 
         return mapping_dict
 
-    def _convert_sheet_name_to_module_name(self, sheet_name: str):
+    def _convert_sheet_name_to_module_name(self, sheet_name: str) -> str:
         """
         Excel sheet names are for humans.
         """
@@ -100,13 +208,55 @@ class Mapping:
 
         return all_sheets
 
-    def export_single_module_as_json_file(self, module_name, mapping_dict):
+    def export_single_module_as_json_file(self, module_name: str, mapping_dict: Dict):
 
         if not os.path.exists(self.json_path):
             os.makedirs(self.json_path)
 
         with open(f"{self.json_path}/{module_name}_mapping.json", "w") as json_out:
             json.dump(mapping_dict, json_out, indent=4)
+
+    def export_summary_as_json_file(self):
+
+        path = f"{self.json_path}/meta"
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open(f"{path}/mapping_progress_summary.json", "w") as json_out:
+            json.dump(self.summary, json_out, indent=4)
+
+    def generate_diff_for_module(self, module_name: str, mapping_dict: Dict):
+        try:
+            with open(
+                f"{self.json_path}/{module_name}_mapping.json", "r"
+            ) as original_file:
+                original_data = original_file.read()
+                original_dict = json.loads(original_data)
+                diff = DeepDiff(original_dict, mapping_dict)
+
+                self.diff[module_name] = diff
+        except Exception:
+            pass
+
+        try:
+            with open(f"{self.json_path}/meta/diff.json", "r") as previous_diff:
+                diff = previous_diff.read()
+                diff_dict = json.loads(diff)
+                previous_date = diff_dict["generated_date"]
+                self.diff["previous_generate_date"] = previous_date
+        except Exception:
+            pass
+
+    def export_diff_as_json_file(self):
+
+        path = f"{self.json_path}/meta"
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open(f"{path}/diff.json", "w") as json_out:
+            json.dump(self.diff, json_out, indent=4)
 
     def generate_json_files(self):
 
@@ -117,11 +267,22 @@ class Mapping:
                 # if "_lookup" not in name:
                 if name in ["client_persons", "client_addresses"]:
                     module_dict = self._clean_up_and_convert_to_dict(df=df)
+
+                    self._add_single_module_details_to_summary(
+                        module_name=name, mapping_dict=module_dict
+                    )
                     if len(module_dict) > 0:
                         module_dict = self._format_multiple_columns(
                             mapping_dict=module_dict
                         )
 
+                        self.generate_diff_for_module(
+                            module_name=name, mapping_dict=module_dict
+                        )
+
                         self.export_single_module_as_json_file(
                             module_name=name, mapping_dict=module_dict
                         )
+        self.export_summary_as_json_file()
+        print(self.diff)
+        self.export_diff_as_json_file()
