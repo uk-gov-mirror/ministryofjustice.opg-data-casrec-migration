c1ad9864c41e701a36752cd7a68ca9f5d98ac4e7
jackgoodby <jackgoodby@gmail.com>
2020-11-16 15:51:56 +0000

    IN-496 rebased

--- migration_steps/acquire_target_ids/app/acquire_target_ids.sh
+++ migration_steps/acquire_target_ids/app/acquire_target_ids.sh
@@ -12,6 +12,7 @@ export PGPASSWORD=${DB_PASSWORD}
 pg_dump -U ${DB_USER} -n etl2 -h ${DB_HOST} -p ${DB_PORT} ${DB_NAME} > "${DIR}/sql/schemas/etl2.sql"
 cat "${DIR}/sql/schemas/etl2.sql" | sed 's/etl2/pre_migrate/' | sed 's/CREATE SCHEMA pre_migrate;/DROP SCHEMA IF EXISTS pre_migrate CASCADE; CREATE SCHEMA pre_migrate;/' > "${DIR}/sql/schemas/pre_migrate.sql"
 psql -U ${DB_USER} -h ${DB_HOST} -p ${DB_PORT} ${DB_NAME} < "${DIR}/sql/schemas/pre_migrate.sql"
+psql -U ${DB_USER} -h ${DB_HOST} -p ${DB_PORT} ${DB_NAME} < "${DIR}/sql/sirius_id_cols.sql"
 #end move
 
 python3 "${DIR}/fixtures.py"
