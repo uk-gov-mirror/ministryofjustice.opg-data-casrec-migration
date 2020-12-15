edb55171e4d33de2088079e573fe2fd38ccad223
jackgoodby <jackgoodby@gmail.com>
2020-12-15 16:58:31 +0000

    Split stage schema creation out of prepare target

--- migration_steps/prepare/create_stage_schema/app/app.py
+++ migration_steps/prepare/create_stage_schema/app/app.py
@@ -6,17 +6,14 @@ current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, str(current_path) + "/../../../shared")
 
 import time
-import psycopg2
 from config2 import get_config
 from dotenv import load_dotenv
-from helpers import log_title
 from db_helpers import *
 import logging
 import custom_logger
 import click
 
 env_path = current_path / "../../../../.env"
-sql_path = current_path / "sql"
 shared_sql_path = current_path / "../../../shared/sql"
 load_dotenv(dotenv_path=env_path)
 
@@ -42,28 +39,17 @@ def set_logging_level(verbose):
 @click.option("-v", "--verbose", count=True)
 def main(verbose):
     set_logging_level(verbose)
-    log.info(log_title(message="Prepare Target"))
 
-    log.info("Perform Sirius DB Housekeeping")
-    conn_target = psycopg2.connect(config.get_db_connection_string("target"))
-    log.debug(
-        "(operations which need to be performed on Sirius DB ahead of the final Casrec Migration)"
+    log.info("Take a fresh copy of the Sirius data structure")
+    copy_schema(
+        log=log,
+        sql_path=shared_sql_path,
+        from_config=config.db_config["target"],
+        from_schema=config.schemas["public"],
+        to_config=config.db_config["migration"],
+        to_schema=config.schemas["pre_migration"],
+        structure_only=True,
     )
-    execute_sql_file(sql_path, "prepare_sirius.sql", conn_target)
-
-    log.info("Roll back previous migration")
-    max_orig_person_id = result_from_sql_file(
-        sql_path, "get_max_orig_person_id.sql", conn_target
-    )
-    execute_generated_sql(
-        sql_path,
-        "rollback_fixtures.template.sql",
-        "{max_orig_person_id}",
-        max_orig_person_id,
-        conn_target,
-    )
-
-    conn_target.close()
 
 
 if __name__ == "__main__":
@@ -72,9 +58,6 @@ if __name__ == "__main__":
     log.setLevel(1)
     log.debug(f"Working in environment: {os.environ.get('ENVIRONMENT')}")
 
-    if environment in ("local", "development"):
-        main()
-    else:
-        log.warning("Skipping step not designed to run on environment %s", environment)
+    main()
 
     print(f"Total time: {round(time.process_time() - t, 2)}")
--- migration_steps/prepare/prepare.sh
+++ migration_steps/prepare/prepare.sh
@@ -3,3 +3,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 python3 "${DIR}/prepare_target/app/app.py"
+python3 "${DIR}/create_stage_schema/app/app.py"
