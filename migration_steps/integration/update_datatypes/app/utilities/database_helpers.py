2d08881013e1b84ed55b7a9dd8b32f1eb5400299
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-04 15:03:03 +0000

    Auto stash before rebase of "master"
    remove big comment

--- migration_steps/integration/update_datatypes/app/utilities/database_helpers.py
+++ migration_steps/integration/update_datatypes/app/utilities/database_helpers.py
@@ -23,58 +23,6 @@ def generate_select_string(mapping_details: Dict, schema: str, table_name: str) 
     return statement
 
 
-# def generate_create_statement(
-#     mapping_details: Dict, schema: str, table_name: str
-# ) -> str:
-#     log.debug(f"Generating table create statement for {table_name}")
-#     statement = f"CREATE TABLE IF NOT EXISTS {schema}.{table_name} (\n"
-#
-#     pg_remap = {
-#         'str': 'text',
-#         'date': 'timestamp',
-#         'datetime': 'timestamp',
-#     }
-#
-#     columns = []
-#     for col, details in mapping_details.items():
-#         details['data_type']  = pg_remap[details['data_type']] if details['data_type'] in pg_remap else details['data_type']
-#         columns.append(f"{col} {details['data_type']}")
-#         if details["is_pk"] is True or details["fk_parents"] is not None:
-#             columns.append(f"sirius_{col} {details['data_type']}")
-#
-#     statement += ", ".join(columns)
-#
-#     statement += ");"
-#     log.log(config.VERBOSE, f"Table create statement: {statement}")
-#     return statement
-
-
-# def generate_mapping_table_create_generic(
-#     mapping_details: Dict, schema: str, table_name: str, entity_name: str
-# ) -> str:
-#     log.debug(
-#         f"Generating mapping table create statement for {entity_name} -"
-#         f" {table_name}"
-#     )
-#     statement = (
-#         f"CREATE TABLE IF NOT EXISTS {schema}.sirius_map_{entity_name}"
-#         f"_{table_name} (\n"
-#     )
-#
-#     columns = []
-#     for col, details in mapping_details.items():
-#         if col in ["caserecnumber"]:
-#             columns.append(f"{col} {details['data_type']}")
-#         if details["is_pk"] is True or len(details["fk_parents"]) > 0:
-#             columns.append(f"sirius_{col} {details['data_type']}")
-#
-#     statement += ", ".join(columns)
-#
-#     statement += ");"
-#     log.log(config.VERBOSE, f"Mapping table create statement: {statement}")
-#     return statement
-
-
 def generate_mapping_table_create(
     schema: str, table_name: str, mapping_columns: Dict
 ) -> str:
