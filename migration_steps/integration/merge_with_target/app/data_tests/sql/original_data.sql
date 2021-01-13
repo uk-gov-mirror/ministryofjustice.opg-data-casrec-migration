dda9da22777879aa6d8b4ac371d804d403b1ed34
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-13 15:02:01 +0000

    supervision level log

--- migration_steps/integration/merge_with_target/app/data_tests/sql/original_data.sql
+++ migration_steps/integration/merge_with_target/app/data_tests/sql/original_data.sql
@@ -2,6 +2,7 @@ select
     pat."Case" as caserecnumber,
     count(pat."Forename") over (partition by pat."Case") as person_count,
     count(pat."Adrs1") over (partition by pat."Case")  as address_count,
-   (select count(*) from schema.order orders where orders."Case" = pat."Case") as case_count
+   (select count(*) from schema.order orders where orders."Case" = pat."Case") as case_count,
+   (select count(*) from schema.order orders where orders."Case" = pat."Case") as supervision_level_log_count
 from schema.pat pat
 where pat."Case" in (test_case_list);
