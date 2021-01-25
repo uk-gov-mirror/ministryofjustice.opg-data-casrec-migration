55ac721cec681619210ec9d7679db56f266de335
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-25 15:22:43 +0000

    wip

---
+++ migration_steps/integration/load_to_staging/app/app.py
@@ -0,0 +1,81 @@
+import sys
+import os
+from pathlib import Path
+
+
+# from utilities.clear_database import clear_tables
+
+
+from move import generate_inserts
+
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, str(current_path) + "/../../../shared")
+
+
+
+import logging
+import time
+import click
+from sqlalchemy import create_engine
+import custom_logger
+from helpers import log_title
+import config2
+from dotenv import load_dotenv
+
+
+# set config
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+env_path = current_path / "../../.env"
+load_dotenv(dotenv_path=env_path)
+
+environment = os.environ.get("ENVIRONMENT")
+config = config2.get_config(env=environment)
+
+# logging
+# custom_logger.custom_log_level(levels=config.custom_log_levels)
+config.custom_log_level()
+verbosity_levels = config.verbosity_levels
+log = logging.getLogger("root")
+log.addHandler(custom_logger.MyHandler())
+
+# database
+db_config = {
+    "db_connection_string": config.get_db_connection_string("migration"),
+    "source_schema": config.schemas["integration"],
+    "target_schema": config.schemas["pre_migration"],
+}
+target_db_engine = create_engine(db_config["db_connection_string"])
+
+
+@click.command()
+@click.option("-v", "--verbose", count=True)
+@click.option(
+    "--clear",
+    prompt=False,
+    default=False,
+    help="Clear existing database tables: True or False",
+)
+def main(verbose, clear):
+    try:
+        log.setLevel(verbosity_levels[verbose])
+        log.info(f"{verbosity_levels[verbose]} logging enabled")
+    except KeyError:
+        log.setLevel("INFO")
+        log.info(f"{verbose} is not a valid verbosity level")
+        log.info(f"INFO logging enabled")
+
+    log.info(log_title(message="Integration Step: Load to Staging"))
+    log.debug(f"Working in environment: {os.environ.get('ENVIRONMENT')}")
+
+    # if clear:
+    #     clear_tables(db_config)
+
+
+    generate_inserts(db_config=db_config, db_engine=target_db_engine)
+
+if __name__ == "__main__":
+    t = time.process_time()
+
+    main()
+
+    print(f"Total time: {round(time.process_time() - t, 2)}")
---
+++ migration_steps/integration/load_to_staging/app/move.py
@@ -0,0 +1,76 @@
+import json
+import os
+import logging
+
+import psycopg2
+from psycopg2 import errors
+
+log = logging.getLogger("root")
+
+
+completed_tables = []
+
+def get_columns_query(table, schema):
+    return f"""
+        SELECT column_name FROM information_schema.columns
+        WHERE table_schema = '{schema}'
+        AND table_name = '{table}';
+        """
+
+
+def add_missing_columns_query(table, schema, columns):
+
+
+    query = f"ALTER TABLE {schema}.{table} "
+    for i, col in enumerate(columns):
+        query += f"ADD COLUMN IF NOT EXISTS {col} TEXT"
+        if i+1 < len(columns):
+            query += ', '
+    query += ';'
+
+
+    return query
+
+
+def generate_inserts(db_config, db_engine):
+
+    path = f"{os.path.dirname(__file__)}/tables.json"
+
+
+    with open(path) as tables_json:
+        tables_list = json.load(tables_json)
+
+
+
+
+    for i, table in enumerate(tables_list):
+        log.info(f"Inserting {table} into {db_config['target_schema']}")
+        log.info(f"{len(completed_tables)}/{len(tables_list)} tables have been completed")
+        log.info(f"This is table number {i+1}")
+        log.debug(f"Completed tables: {', '.join(completed_tables)}")
+        log.debug(f"Not completed tables: {', '.join(tables_list)}")
+
+        get_source_cols_query = get_columns_query(table=table, schema=db_config['source_schema'])
+        get_target_cols_query = get_columns_query(table=table, schema=db_config['target_schema'])
+
+        source_columns = [x[0] for x in db_engine.execute(get_source_cols_query).fetchall()]
+        target_columns = [x[0] for x in db_engine.execute(get_target_cols_query).fetchall()]
+        columns_missing_from_target = list(set(source_columns) - set(target_columns))
+
+        if len(columns_missing_from_target) > 0:
+            alter_target_query = add_missing_columns_query(table=table, schema=db_config['target_schema'], columns=columns_missing_from_target)
+
+
+            db_engine.execute(alter_target_query)
+
+        query = f"""
+        INSERT INTO {db_config["target_schema"]}.{table} ({', '.join(source_columns)})
+        SELECT {', '.join(source_columns)} FROM {db_config["source_schema"]}.{table};
+        """
+
+        try:
+            db_engine.execute(query)
+            completed_tables.append(table)
+        except Exception as e:
+            log.error(f"There was an error inserting {table} into {db_config['target_schema']}")
+            break
