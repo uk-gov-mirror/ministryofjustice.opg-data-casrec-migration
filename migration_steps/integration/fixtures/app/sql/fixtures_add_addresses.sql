138f87c4cfdbeb95e5ade4cd94773144bd3afe00
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-27 09:30:53 +0000

    tweak fixtures so they don't error on null values

--- migration_steps/integration/fixtures/app/sql/fixtures_add_addresses.sql
+++ migration_steps/integration/fixtures/app/sql/fixtures_add_addresses.sql
@@ -1,5 +1,6 @@
-INSERT INTO addresses (person_id, address_lines, isairmailrequired) (
-    SELECT id as person_id, '["m","o","o"]' as address_lines, CAST(0 AS BOOLEAN) as isairmailrequired
+INSERT INTO addresses (id, person_id, address_lines, isairmailrequired) (
+    SELECT (row_number() over ( order by id))*1000 as id,
+           id as person_id, '["m","o","o"]' as address_lines, CAST(0 AS BOOLEAN) as isairmailrequired
     FROM persons
     WHERE clientsource = 'SKELETON'
     ORDER BY id DESC
