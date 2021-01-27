07fe4124788e2981d5ce029fc3b35dc9e3007165
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-27 09:30:53 +0000

    tweak fixtures so they don't error on null values

--- migration_steps/integration/fixtures/app/entities/client.py
+++ migration_steps/integration/fixtures/app/entities/client.py
@@ -6,6 +6,7 @@ from db_helpers import (
     execute_sql_file,
 )
 from pathlib import Path
+import random
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 sql_path = current_path / "../sql"
@@ -20,6 +21,10 @@ def load_fixtures(config, conn_migration, conn_target):
         sql_path, "fixtures_select_clients.sql", conn_migration, schema
     )
     persons_df["uid"] = list(range(max_person_uid + 1, max_person_uid + 11, 1))
+    persons_df["id"] = list(range(1000, 1010, 1))
+    persons_df["correspondencebypost"] = False
+    persons_df["correspondencebyphone"] = False
+    persons_df["correspondencebyemail"] = False
     execute_insert(conn_target, persons_df, "persons")
 
 
