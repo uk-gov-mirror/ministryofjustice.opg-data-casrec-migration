9b5e4c1bbca9ffd0c3308f8e33fba4b73c0a2161
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-03-09 13:06:58 +0000

    main `notes` table, however removed `keydate` until I know what format it's supposed to be

--- migration_steps/transform_casrec/app/entities/remarks/__init__.py
+++ migration_steps/transform_casrec/app/entities/remarks/__init__.py
@@ -3,22 +3,25 @@ import logging
 
 from helpers import log_title
 
+from entities.remarks.notes import insert_notes
+
 log = logging.getLogger("root")
 
 
 def runner(target_db, db_config):
     """
-    | Name      | Running Order | Requires |
-    | --------- | ------------- | -------- |
-    |           |               |          |
-    |           |               |          |
-    |           |               |          |
+    | Name          | Running Order  | Requires     |
+    | ------------- | -------------- | ------------ |
+    | notes         | 1              |              |
+    | caseitem_note | 2              | notes, cases |
+    |               |                |              |
 
     """
 
     log.info(log_title(message="remarks"))
 
-    log.debug("Not currently implemented")
+    log.debug("insert_notes")
+    insert_notes(target_db=target_db, db_config=db_config)
 
 
 if __name__ == "__main__":
---
+++ migration_steps/transform_casrec/app/entities/remarks/notes.py
@@ -0,0 +1,24 @@
+from utilities.basic_data_table import get_basic_data_table
+
+definition = {
+    "source_table_name": "remarks",
+    "source_table_additional_columns": [],
+    "destination_table_name": "notes",
+}
+
+mapping_file_name = "notes_mapping"
+
+
+def insert_notes(db_config, target_db):
+
+    sirius_details, persons_df = get_basic_data_table(
+        db_config=db_config,
+        mapping_file_name=mapping_file_name,
+        table_definition=definition,
+    )
+
+    target_db.insert_data(
+        table_name=definition["destination_table_name"],
+        df=persons_df,
+        sirius_details=sirius_details,
+    )
