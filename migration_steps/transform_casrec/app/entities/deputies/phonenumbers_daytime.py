6645a0ed19dd6c8c066a7eda3bdaa2b471f19f16
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-03-12 13:20:37 +0000

    chunk deputies

--- migration_steps/transform_casrec/app/entities/deputies/phonenumbers_daytime.py
+++ migration_steps/transform_casrec/app/entities/deputies/phonenumbers_daytime.py
@@ -12,12 +12,9 @@ mapping_file_name = "deputy_daytime_phonenumbers_mapping"
 
 def insert_phonenumbers_deputies_daytime(db_config, target_db):
 
-    sirius_details, phonenos_df = get_basic_data_table(
-        db_config=db_config,
-        mapping_file_name=mapping_file_name,
-        table_definition=definition,
-    )
-
+    chunk_size = db_config["chunk_size"]
+    offset = 0
+    chunk_no = 1
     persons_query = (
         f'select "id", "email" from {db_config["target_schema"]}.persons '
         f"where \"type\" = 'actor_deputy';"
@@ -26,24 +23,39 @@ def insert_phonenumbers_deputies_daytime(db_config, target_db):
 
     persons_df = persons_df[["id", "email"]]
 
-    phonenos_joined_df = phonenos_df.merge(
-        persons_df, how="left", left_on="c_email", right_on="email"
-    )
+    while True:
+        try:
 
-    phonenos_joined_df["person_id"] = phonenos_joined_df["id_y"]
-    phonenos_joined_df = phonenos_joined_df.drop(columns=["id_y", "email"])
-    phonenos_joined_df = phonenos_joined_df.rename(columns={"id_x": "id"})
+            sirius_details, phonenos_df = get_basic_data_table(
+                db_config=db_config,
+                mapping_file_name=mapping_file_name,
+                table_definition=definition,
+                chunk_details={"chunk_size": chunk_size, "offset": offset},
+            )
 
-    phonenos_joined_df["person_id"] = (
-        phonenos_joined_df["person_id"]
-        .fillna(0)
-        .astype(int)
-        .astype(object)
-        .where(phonenos_joined_df["person_id"].notnull())
-    )
+            phonenos_joined_df = phonenos_df.merge(
+                persons_df, how="left", left_on="c_email", right_on="email"
+            )
 
-    target_db.insert_data(
-        table_name=definition["destination_table_name"],
-        df=phonenos_joined_df,
-        sirius_details=sirius_details,
-    )
+            phonenos_joined_df["person_id"] = phonenos_joined_df["id_y"]
+            phonenos_joined_df = phonenos_joined_df.drop(columns=["id_y", "email"])
+            phonenos_joined_df = phonenos_joined_df.rename(columns={"id_x": "id"})
+
+            phonenos_joined_df["person_id"] = (
+                phonenos_joined_df["person_id"]
+                .fillna(0)
+                .astype(int)
+                .astype(object)
+                .where(phonenos_joined_df["person_id"].notnull())
+            )
+
+            target_db.insert_data(
+                table_name=definition["destination_table_name"],
+                df=phonenos_joined_df,
+                sirius_details=sirius_details,
+                chunk_no=chunk_no,
+            )
+            offset += chunk_size
+            chunk_no += 1
+        except Exception:
+            break
